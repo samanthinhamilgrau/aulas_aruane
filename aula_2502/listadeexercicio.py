e8bff118d6d671e0504d5fa0e8cf515412dfa927
Samanthinha <samanthapcaetano809@gmail.com>
2025-02-25 22:21:00 -0300

    Aula do dia 25

---
+++ aula_2502/listadeexercicio.py
@@ -0,0 +1,38 @@
+# Exercicio 1
+
+print("Exercicio 1")
+
+nome = input("Qaul seu nome?")
+print(f"Bem vindo(a) {nome}")
+
+# Exercicio 2
+
+print("Exercicio 2")
+
+palavra1 = input("Insira a  primeira palavra:")
+palavra2 = input("Insira a  segunda palavra:")
+
+resultado = palavra1 + " " + palavra2
+
+print(f"Palavras juntas: {resultado}")
+
+# Exercicio 3
+
+print("Exercicio 3")
+
+frase = input("Insira uma frase:")
+qtdddepalavra = len(frase.split())
+
+print(f"Qauantidade de palavras: {qtdddepalavra}")
+
+# Exercicio 4
+
+print("Exercicio 4")
+
+numero1 = int(input("Insira o primerio numero:"))
+numero2 = int(input("Insira o segundo numero:"))
+
+soma = numero1 + numero2
+
+
+print(f"Soma dos numero: {soma}")
